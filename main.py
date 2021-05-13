9b0de435e9a17b7447c2d2e1cc355b4dbfc2f046
ryan-kanghyun-moon <kanghyun323@gmail.com>
2021-05-13 02:18:25 -0700

    only add \n where needed, need towork on edge cases

--- main.py
+++ main.py
@@ -8,15 +8,66 @@ URL3 = 'https://missa.cbck.or.kr/DailyMissa/20210502'
 page = requests.get(URL3)
 soup = BeautifulSoup(page.content, 'html.parser')
 
+def insert_n(verse, i):
+    return verse[:i] + "\n" + verse[i:]
+
+
+def is_integer(n):
+    try:
+        int(n)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def get_start(v):
+    return v[str.find(v, ",") + 1: str.find(v, "-")]
+
+def get_verse(raw_bib):
+    no_n = ''.join(raw_bib.splitlines())
+   
+    verse = no_n[str.find(no_n, ">") + 1:]
+
+    # i = str.find(verse, "-") + 1
+    
+    # while is_integer(verse[i]): 
+    #     i += 1
+    
+    # verse = insert_n(verse, i)
+    # i += 1
+
+    
+    
+    s = get_start(verse)
+    i = str.find(verse, "," + s) + 2
+    print(str(i) + "\n")
+    
+    temp = insert_n(verse[i:], str.find(verse[i:], s))
+    verse = verse[:i] + temp
+    
+    
+
+
+    while i < len(verse):
+        if is_integer(verse[i]) and not is_integer(verse[i-1]):
+            verse = insert_n(verse, i)
+            i += 1
+        i += 1
+
+    return verse
+
+
 def main():
    
    
-    string = repr(soup.text)
-    
-    string = string[str.find(string, "복음 환호송"): str.find(string, "그리스도님 찬미합니다") + len("그리스도님 찬미합니다.")]
 
-    print(string)
-   
+    string = soup.text
+
+    string = string[str.find(string, "복음 환호송"): str.find(string, "그리스도님 찬미합니다") + len("그리스도님 찬미합니다.")]
+    
+    verse = get_verse(string)
+
+    print(verse)
 
     
 if __name__ == "__main__":
